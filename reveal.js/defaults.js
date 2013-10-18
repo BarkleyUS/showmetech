8de055f903c97927c45b791f2ea760a689faddc0
Alan Christopher Thomas <alan@alanct.com>
2013-10-18 14:43:31 -0500

    use github.io to host assets instead of rawgithub

--- reveal.js/defaults.js
+++ reveal.js/defaults.js
@@ -11,11 +11,11 @@ Reveal.initialize({
 
     // Optional libraries used to extend on reveal.js
     dependencies: [
-        { src: 'https://rawgithub.com/BarkleyUS/showmetech/gh-pages/reveal.js/lib/js/classList.js', condition: function() { return !document.body.classList; } },
-        { src: 'https://rawgithub.com/BarkleyUS/showmetech/gh-pages/reveal.js/plugin/markdown/marked.js', condition: function() { return !!document.querySelector( '[data-markdown]' ); } },
-        { src: 'https://rawgithub.com/BarkleyUS/showmetech/gh-pages/reveal.js/plugin/markdown/markdown.js', condition: function() { return !!document.querySelector( '[data-markdown]' ); } },
-        { src: 'https://rawgithub.com/BarkleyUS/showmetech/gh-pages/reveal.js/plugin/highlight/highlight.js', async: true, callback: function() { hljs.initHighlightingOnLoad(); } },
-        { src: 'https://rawgithub.com/BarkleyUS/showmetech/gh-pages/reveal.js/plugin/zoom-js/zoom.js', async: true, condition: function() { return !!document.body.classList; } },
-        { src: 'https://rawgithub.com/BarkleyUS/showmetech/gh-pages/reveal.js/plugin/notes/notes.js', async: true, condition: function() { return !!document.body.classList; } }
+        { src: 'http://barkleyus.github.io/showmetech/reveal.js/lib/js/classList.js', condition: function() { return !document.body.classList; } },
+        { src: 'http://barkleyus.github.io/showmetech/reveal.js/plugin/markdown/marked.js', condition: function() { return !!document.querySelector( '[data-markdown]' ); } },
+        { src: 'http://barkleyus.github.io/showmetech/reveal.js/plugin/markdown/markdown.js', condition: function() { return !!document.querySelector( '[data-markdown]' ); } },
+        { src: 'http://barkleyus.github.io/showmetech/reveal.js/plugin/highlight/highlight.js', async: true, callback: function() { hljs.initHighlightingOnLoad(); } },
+        { src: 'http://barkleyus.github.io/showmetech/reveal.js/plugin/zoom-js/zoom.js', async: true, condition: function() { return !!document.body.classList; } },
+        { src: 'http://barkleyus.github.io/showmetech/reveal.js/plugin/notes/notes.js', async: true, condition: function() { return !!document.body.classList; } }
     ]
 });
